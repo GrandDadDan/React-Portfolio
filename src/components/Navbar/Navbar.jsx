d129617d6dbe8e4222e6d1fae8cb4f2fb2644876
Daniel <dpengmarais99@gmail.com>
2025-01-24 20:55:18 +0200

    Added navbar and dropdown menu. Started Hero section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./Navbar.module.css"
+import {getImageUrl} from "../../utils"
 
 export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
   return <nav className={styles.navbar}>
     <a href="/" className={styles.title}>Portfolio</a>
     <div className={styles.menu}>
-        <ul className={styles.menuItems}>
+    <img className = {styles.menuBtn} src={menuOpen ? getImageUrl("closeIcon.png"):getImageUrl("menuIcon.png") } alt ="menu-button" onClick= {() => setMenuOpen(!menuOpen) }/>
+        <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`} onClick={() => setMenuOpen(false)}>
             <li><a href="#about">About</a></li>
             <li><a href="#experience">Experience</a></li>
             <li><a href="#projects">Projects</a></li>
             <li><a href="#projects">Contact</a></li>
+            
         </ul>
+        
     </div>
   </nav>
   
