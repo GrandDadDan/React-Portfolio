a521ba6223f095644014ba80b4ab219c306ea96b
Daniel <dpengmarais99@gmail.com>
2025-01-27 07:46:38 +0200

    Completed the about section and made it responsive. Started on the experience section.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,25 +1,40 @@
 import React from 'react'
 import { getImageUrl } from '../../utils'
+import styles from "./About.module.css"
 
 export const About = () => {
-  return <section>
-    <h2>About</h2> 
-    <div><img src={getImageUrl("aboutImage.png")} width= {400} alt="about" /></div>
-    <ul>
-        <li><img src={getImageUrl("htmlImage.png")} width={100} alt="" />
-        <div>
+  return <section className={styles.container} id="about">
+    <h2 className={styles.title}>About</h2> 
+    <div className={styles.content}>
+        <img src={getImageUrl("aboutImage.png")} 
+        width= {400} 
+        alt="about"
+        className={styles.aboutImage} />
+    <ul className={styles.aboutItems}>
+        <li className={styles.aboutItem}>
+            <img src={getImageUrl("htmlImage.png")} width={100} alt="" />
+        <div className={styles.aboutItemText}>
         <h3>Frontend Developer</h3>
-        <p>I am a self-taught frontend developer with freelancing experience in building responsive and optimized sites</p>
+        <p>I am a self-taught frontend developer with freelancing experience in building responsive and optimized sites.</p>
         </div>
             </li>
-        <li><img src={getImageUrl("serverImage.png")} width={100} alt="" />
-        <div>
+        <li className={styles.aboutItem}>
+            <img src={getImageUrl("serverImage.png")} width={100} alt="" />
+        <div className={styles.aboutItemText}> 
         <h3>Backend Developer</h3>
-        <p></p>
+        <p>I am a self taught developer in building server side applications using APIs and databases.</p>
         </div>
+            </li >
+        <li className={styles.aboutItem}>
+           
+            <img src={getImageUrl("cssImage.png")} width={100} alt="" />
+            <div className={styles.aboutItemText}>
+            <h3>UI Design</h3>
+            <p>Strong attention to detail in designing beautiful websites though CSS and Bootstrap, and the creativity of motion graphic design.</p>
+            </div>
             </li>
-        <li><img src={getImageUrl("cssImage.png")} width={100} alt="" /></li>
     </ul>
+    </div>
   </section>
 }
 
