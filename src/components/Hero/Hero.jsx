5c7b25f919140cf0e9112edefc7a3e2a29162747
Daniel <dpengmarais99@gmail.com>
2025-01-28 15:02:04 +0200

    Built and deployed porportfolio to netlify

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
   return <section className={styles.container}>
     <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Daniel!</h1>
-        <p className={styles.description}>Postgraduate student, motion graphics designer, and freelance fullstack developer with 2 years exerperience using React and Node.JS</p>
+        <p className={styles.description}>Postgraduate student, motion graphics designer, fullstack developer with 3 years hands-on exerperience using React and Node.JS</p>
         <a href="mailto:dpengmarais99@gmail.com" className={styles.contactBtn}>Email Me</a>
         <a href={resume} download = 'resume' className={styles.resumeBtn}>CV<img src={getImageUrl("download.png")} alt="" width={50} /></a>
     </div>
